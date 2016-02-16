2f1ef78de5d358e5ef5ebf691165c1f2af8a2dcd
FrankGwada <franciskipchumba5@gmail.com>
2016-02-16 15:50:38 +0000

    add more functonality to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from blog.models import Blog, Category
+from djangorocks.blog.models import Blog, Category
 
-admin.site.register(Blog)
-admin.site.register(Category)
+class BlogAdmin(admin.ModelAdmin):
+    exclude = ['posted']
+    prepopulated_fields = {'Slug': ('title')}
+    
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields= {'slug':('title')
+    
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 # Register your models here.
